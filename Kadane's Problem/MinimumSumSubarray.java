560b310a64a0858f2f7c89ef229dc4a79809dc3e
ANUSHKASONI2099 <153723294+ANUSHKASONI2099@users.noreply.github.com>
2026-03-23 20:14:06 +0530

    Update MinimumSumSubarray.java

---
+++ Kadane's Problem/MinimumSumSubarray.java
@@ -0,0 +1,14 @@
+class MinimumSumSubarray{
+    public int minSubarray(int[]  a){
+        int bestEnding = a[0];
+        int ans = a[0];
+        for(int i = 1; i< a.length ; i++){
+            int v1 = bestEnding + a[i];
+            int v2 = a[i];
+            bestEnding = Math.min(  v1 , v2);
+            ans = Math.min(ans , bestEnding);
+        }
+        return ans;
+    }
+
+}
