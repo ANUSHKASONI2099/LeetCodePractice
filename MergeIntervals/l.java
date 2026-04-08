32873e726e1ad543cd576c6441ebe077c8af6b03
ANUSHKASONI2099 <153723294+ANUSHKASONI2099@users.noreply.github.com>
2026-04-08 23:45:18 +0530

    Create l.java

---
+++ MergeIntervals/l.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public int[][] insert(int[][] arr, int[] newInterval) {
+        List<int[]> ans = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            // Case 1: no overlap, current left side
+            if (arr[i][1] < newInterval[0]) {
+                ans.add(arr[i]);
+            } // Case 2: no overlap, current right side
+            else if (arr[i][0] > newInterval[1]) {
+                ans.add(newInterval);
+                newInterval = arr[i];
+            } // Case 3: overlap → merge
+            else {
+                newInterval[0] = Math.min(newInterval[0], arr[i][0]);
+                newInterval[1] = Math.max(newInterval[1], arr[i][1]);
+            }
+        }
+
+        ans.add(newInterval);
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
